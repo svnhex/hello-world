9542ee95bc0b88409b4bf49bc5ff30766229d3c0
Edgar Landazuri <lastzytex@gmail.com>
2019-04-10 13:13:09 -0500

    Initial commit using the Tortoise Git...

---
+++ Hello-World.Web.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hello_World.Web.Tests
+{
+   [TestClass]
+   public class UnitTest1
+   {
+      [TestMethod]
+      public void TestMethod1()
+      {
+      }
+   }
+}
